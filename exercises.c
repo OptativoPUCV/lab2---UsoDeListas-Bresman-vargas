f4c74d64582cfe070ef0f66a5354196425acd12d
Your Name <you@example.com>
2024-03-23 19:11:51 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -101,7 +101,7 @@ void copia_pila(Stack* P1, Stack* P2) {
       push(P3, dato);
       push(P2, dato);
    } 
-   while((dato = pop(P3)) != NULL){
+   while((dato_copia = pop(P3)) != NULL){
       push(P2, dato_copia);
    }
   
